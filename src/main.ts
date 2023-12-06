8614ed0292ffdeea0acc84745a0fcde22417bc26
ValentinViennot <valentinviennot@users.noreply.github.com>
2023-12-06 22:41:19 +0000

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMatomo from 'vue-matomo'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(VueMatomo, {
+    host: 'https://analytics.unstaticlabs.com',
+    siteId: 1,
+    cookieDomain: "*.fuckthevcs.com",
+    domains: ["*.fuckthevcs.com"],
+    router
+})
 
 app.mount('#app')
